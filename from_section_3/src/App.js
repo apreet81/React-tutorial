8cb5466d258a8aed0834e072a5de409d5b151e69
Amanpreet <amanpreet.singh@mymindinfotech.com>
2020-01-28 19:01:21 +0530

    Completed till video 5.11

--- from_section_3/src/App.js
+++ from_section_3/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./App.css";
+import classes from "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
@@ -42,16 +42,8 @@ class App extends Component {
   };
 
   render() {
-    const style = {
-      backgroundColor: "green",
-      color: "white",
-      font: "inherit",
-      border: "1px solid blue",
-      padding: "8px",
-      cursor: "pointer"
-    };
-
     let persons = null;
+    let btnClass='';
     if (this.state.showPersons) {
       persons = (
         <div>
@@ -68,26 +60,25 @@ class App extends Component {
           })}
         </div>
       );
-      style.backgroundColor = "red";
-      style.color = "white";
+      btnClass=classes.Red;
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push("red"); // classes = ['red']
+      assignedClasses.push(classes.red); // classes = ['red']
     }
     if (this.state.persons.length <= 1) {
-      classes.push("bold"); // classes = ['red' , 'bold']
+      assignedClasses.push(classes.bold); // classes = ['red' , 'bold']
     }
     return (
-        <div className="App">
-          <h1>Hi, I am react app</h1>
-          <p className={classes.join(" ")}>This is really working!!!</p>
-          <button style={style} onClick={this.togglePersonHandler}>
-            Toggle Persons
-          </button>
-          {persons}
-        </div>
+      <div className={classes.App}>
+        <h1>Hi, I am react app</h1>
+        <p className={assignedClasses.join(" ")}>This is really working!!!</p>
+        <button className={btnClass} onClick={this.togglePersonHandler}>
+          Toggle Persons
+        </button>
+        {persons}
+      </div>
     );
   }
 }
