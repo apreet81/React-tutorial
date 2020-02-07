b0817330a064d5ffece7016b2cb906c606916e9d
Amanpreet <amanpreet.singh@mymindinfotech.com>
2020-02-07 20:02:59 +0530

    Completed till video 8.8

--- from_section_8_burger_app/src/App.js
+++ from_section_8_burger_app/src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import classes from './App.css';
-
-function App() {
-  return (
-    <div className={classes.App}>
-      <header className={classes.App_header}>
-        <img src={logo} className={classes.App_logo} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className={classes.App_link}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
+      </div>
+    );
+  }
 }
-
 export default App;
