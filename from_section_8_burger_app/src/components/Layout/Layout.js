bb4cc0b4b9f5857ccb69d648c71e31b31bbb17b7
Amanpreet <apreet81@live.com>
2020-02-17 00:50:47 +0530

    Completed till video 8.30

--- from_section_8_burger_app/src/components/Layout/Layout.js
+++ from_section_8_burger_app/src/components/Layout/Layout.js
@@ -1,14 +1,16 @@
 import React from "react";
 
 import Aux from "../../hoc/Auxiliary";
-import Toolbar from '../Navigation/Toolbar/Toolbar'
-import classes from "./Layout.css"
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
+import classes from "./Layout.css";
 
 const layout = props => {
   return (
     <Aux>
       {/* <div>Toolbar, SideDrawer, Backdrop</div> */}
-      <Toolbar/>
+      <Toolbar />
+      <SideDrawer/>
       <main className={classes.Content}>{props.children}</main>
     </Aux>
   );
--- from_section_8_burger_app/src/components/Navigation/Toolbar/Toolbar.js
+++ from_section_8_burger_app/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import Logo from '../../Logo/Logo'
+import React from "react";
 
-import classes from './Toolbar.css';
-const toolbar=(porps)=>(
-    <header className={classes.Toolbar}>
-        <div>MENU</div>
-        <Logo></Logo>
-        <nav>
-            ...
-        </nav>
-    </header>
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+
+import classes from "./Toolbar.css";
+const toolbar = porps => (
+  <header className={classes.Toolbar}>
+    <div>MENU</div>
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
+    <nav>
+      <NavigationItems />
+    </nav>
+  </header>
 );
 
 export default toolbar;
