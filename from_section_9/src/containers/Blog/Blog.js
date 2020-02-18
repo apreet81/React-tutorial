5bed6997af7d1ea84bf4c354a687dcbae9ee78a3
Amanpreet <apreet81@live.com>
2020-02-19 00:44:52 +0530

    Completed till video 9.5

---
+++ from_section_9/src/containers/Blog/Blog.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+import Post from "../../components/Post/Post";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+import "./Blog.css";
+
+class Blog extends Component {
+  state = {
+    posts: []
+  };
+  componentDidMount() {
+    axios
+      .get("https://my-json-server.typicode.com/typicode/demo/posts")
+      .then(response => {
+        this.setState({ posts: response.data });
+        console.log(response);
+      });
+  }
+  render() {
+    const posts = this.state.posts.map(post => {
+      return <Post key={post.id} title={post.title} />;
+    });
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <section>
+          <FullPost />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Blog;
