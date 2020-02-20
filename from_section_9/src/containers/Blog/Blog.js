f29e085f64022a4d1a18c87c56dc9ce76f34719e
Amanpreet <amanpreet.singh@mymindinfotech.com>
2020-02-20 20:35:34 +0530

    Completed video till 9.8

--- from_section_9/src/containers/Blog/Blog.js
+++ from_section_9/src/containers/Blog/Blog.js
@@ -8,25 +8,42 @@ import "./Blog.css";
 
 class Blog extends Component {
   state = {
-    posts: []
+    posts: [],
+    selectedPostId: null
   };
   componentDidMount() {
-    axios
-      .get("https://my-json-server.typicode.com/typicode/demo/posts")
-      .then(response => {
-        this.setState({ posts: response.data });
-        console.log(response);
+    axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
+      const posts = response.data.slice(0, 4);
+      const updatedPosts = posts.map(post => {
+        return {
+          ...post,
+          author: "Max"
+        };
       });
+      this.setState({ posts: updatedPosts });
+      console.log(response);
+    });
   }
+
+  postSelectedHandler = id => {
+    this.setState({ selectedPostId: id });
+  };
   render() {
     const posts = this.state.posts.map(post => {
-      return <Post key={post.id} title={post.title} />;
+      return (
+        <Post
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          clicked={() => this.postSelectedHandler(post.id)}
+        />
+      );
     });
     return (
       <div>
         <section className="Posts">{posts}</section>
         <section>
-          <FullPost />
+          <FullPost id={this.state.selectedPostId} />
         </section>
         <section>
           <NewPost />
