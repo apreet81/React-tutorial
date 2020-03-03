48398b7b9a184bf4b6b6d17248643cada6974acb
Amanpreet <amanpreet.singh@mymindinfotech.com>
2020-03-03 21:12:32 +0530

    Completed till video 12.3

--- from_section_12_burger_app_with_routing/src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ from_section_12_burger_app_with_routing/src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -2,16 +2,21 @@ import React from "react";
 
 import Burger from "../../Burger/Burger";
 import Button from "../../UI/Button/Button";
+import classes from "./CheckoutSummary.css";
 
 const checkoutSummary = props => {
   return (
-    <div>
+    <div className={classes.CheckoutSummary}>
       <h1>Hope it tastes well!</h1>
-      <div style={{ width: "300px", height: "300px", margin: "auto" }}>
+      <div style={{ width: "100%", margin: "auto" }}>
         <Burger ingredients={props.ingredients} />
       </div>
-      <Button btnType="Danger" clicked>CANCEL</Button>
-      <Button btnType="Success" clicked>CONTINUE</Button>
+      <Button btnType="Danger" clicked>
+        CANCEL
+      </Button>
+      <Button btnType="Success" clicked>
+        CONTINUE
+      </Button>
     </div>
   );
 };
