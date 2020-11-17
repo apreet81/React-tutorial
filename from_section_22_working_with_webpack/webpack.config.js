c32fdddc089e9cb6da7a76652c1206937c5a47c7
amanpreet92 <amanpreet.singh@mymindinfotech.com>
2020-11-17 21:04:16 +0530

    Fixed issues.

--- from_section_22_working_with_webpack/webpack.config.js
+++ from_section_22_working_with_webpack/webpack.config.js
@@ -1,57 +1,57 @@
-const path = require("path");
-const autoprefixer=require("autoprefixer");
-const HtmlWebpackPlugin=require("html-webpack-plugin");
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
+  mode: 'development',
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    publicPath: "",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: ''
   },
-  devtools: "cheap-module-eval-source-map",
+  devtool: 'cheap-module-eval-source-map',
   module: {
     rules: [
       {
         test: /\.js$/,
-        loader: "babel-loader",
-        exclude: /node_module/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
       },
       {
         test: /\.css$/,
+        exclude: /node_modules/,
         use: [
-          { loader: "style-loader" },
+          { loader: 'style-loader' },
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
               importLoaders: 1,
               modules: {
-                localIdentName: "[name]_[local]_[hash:base64:5]",
-              },
-            },
+                localIdentName: '[name]__[local]__[hash:base64:5]'
+              }
+            }
           },
           {
-              loader:"postcss-loader",
-              options:{
-                  ident:"postcss",
-                  plugins:()=>[autoprefixer()]
-              }
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [autoprefixer()]
+            }
           }
-        ],
-        exclude: /node_module/,
+        ]
       },
       {
-          test:/\.(png|jpe?g|gif)$/,
-          loader:'url-loader?limit=8000&name=images/[name].[ext]'
+        test: /\.(png|jpe?g|gif)$/,
+        loader: 'url-loader?limit=8000&name=images/[name].[ext]'
       }
-    ],
+    ]
   },
-  plugins:[
-      new HtmlWebpackPlugin({
-          template:__dirname+"/src/index.html",
-          filename:"index.html",
-          inject:"body"
-      })
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + '/src/index.html',
+      filename: 'index.html',
+      inject: 'body'
+    })
   ]
 };
